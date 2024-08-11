247fceb5df0e65a557db10692f9c18524b923b8c
Swaraj Anand <swrjnnd@gmail.com>
2024-08-12 02:30:16 +0530

    Added homcomponent css

--- src/app/store/user.selector.ts
+++ src/app/store/user.selector.ts
@@ -2,10 +2,8 @@ import { createSelector } from '@ngrx/store';
 import { AppState } from './globalUsers.state';
 import { UserState } from './user.reducer';
 
-// Select the user state from the AppState
 export const selectUserState = (state: AppState) => state.user;
 
-// Selector to get the list of existing users
 export const selectExistingUsers = createSelector(
   selectUserState,
   (userState: UserState) => userState.existingUsers
